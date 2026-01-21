bcc2adb9b85eea524dd897cab1976d8bce42befa
unknown <yossaran.p6.2.17.ccs@gmail.com>
2026-01-21 16:49:07 +0700

    p04

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,25 @@
 // Write your code below
+
+const arg3: string = process.argv[2];
+const num1: number = Number(arg3);
+
+
+let pattern = "";
+if (num1 < 1 || isNaN(num1) || arg3 == undefined || arg3 == null || !arg3 || isNaN(num1) === false) {
+
+}
+for (let i = 1; i <= num1; i++) {
+    for (let j = 1; j <= (num1-i); j++) {
+        pattern += " ";
+    }
+
+    for (let k = i ; k > 0; k -= 1) {
+        pattern += k;
+    }
+    pattern += "\n";
+}
+
+console.log(pattern); 
+
+
+
--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,5 +1,15 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
+export function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   // Write your code below
+  
+  const merged = [...arr1, ... arr2]
+  let b: number[];
+  for (let i = 0; i <= merged.length; i++) {
+    if (b.indexOf(merged[i])) {
+      b.push(merged[i]);
+    }
+  }
+  console.log(b);
+  return b;
 }
 
 const arr1 = [1, 2, 3, 4];
